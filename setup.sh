663f254ac540de14823de55c4f89121dd6504ebd
Bruce Teng <torai.d.l@outlook.com>
2024-10-10 10:21:19 +0800

    Update: Add requirements for new packing method.

--- setup.sh
+++ setup.sh
@@ -11,9 +11,9 @@ if [ "$(uname -m)" == "x86_64" ] && [  "$(uname)" == "Linux" ];then
     echo "Device arch: Linux x86_64"
     apt update -y
     apt upgrade -y
-    apt install -y aria2 python3 busybox zip unzip p7zip-full openjdk-8-jre zipalign zstd bc android-sdk-libsparse-utils xmlstarlet
+    apt install -y aria2 python3 busybox zip unzip p7zip-full openjdk-8-jre zipalign zstd bc android-sdk-libsparse-utils xmlstarlet openssl python3-protobuf
     if [ $? -ne 0 ];then
-        echo "安装可能出错，请手动执行：apt install -y aria2 python3 busybox zip unzip p7zip-full openjdk-8-jre zipalign zstd bc xmlstarlet"
+        echo "安装可能出错，请手动执行：sudo apt install -y aria2 python3 busybox zip unzip p7zip-full openjdk-8-jre zipalign zstd bc xmlstarlet openssl python3-protobuf"
     fi
 fi
 
